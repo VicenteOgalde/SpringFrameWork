119e12d028138e69274a07cbcb15aaee728dd39a
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-20 13:18:22 -0300

    practice spring video n8

--- SpringOne/src/cl/vicoga/IoC/EmployeeImpl.java
+++ SpringOne/src/cl/vicoga/IoC/EmployeeImpl.java
@@ -1,13 +1,20 @@
 package cl.vicoga.IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmployeeImpl {
 
 	public static void main(String[] args) {
-		
+		/*
 		Employee emp= new EmployeeClerk();
 		
 		System.out.println(emp.getTask());
-
+	*/
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp=context.getBean("meEmployee",Employee.class);
+		System.out.println(emp.getTask());
+		context.close();
 	}
 
 }
