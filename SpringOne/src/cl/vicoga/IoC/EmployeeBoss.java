d17d39ff9d8683c7d16a78ae5c60787717fcd8f1
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-21 12:54:56 -0300

    practice spring video n12

--- SpringOne/src/cl/vicoga/IoC/EmployeeBoss.java
+++ SpringOne/src/cl/vicoga/IoC/EmployeeBoss.java
@@ -3,6 +3,9 @@ package cl.vicoga.IoC;
 public class EmployeeBoss implements Employee{
 
 	private ReportCreation reportCreation;
+	private String email;
+	private String nameCompany;
+
 	
 	public EmployeeBoss(ReportCreation reportCreation) {
 		this.reportCreation=reportCreation;
@@ -16,5 +19,21 @@ public class EmployeeBoss implements Employee{
 	public String getReport() {
 		// TODO Auto-generated method stub
 		return "boss report: "+ reportCreation.getReport();
+		
+	}
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNameCompany() {
+		return nameCompany;
+	}
+
+	public void setNameCompany(String nameCompany) {
+		this.nameCompany = nameCompany;
 	}
 }
--- SpringOne/src/cl/vicoga/IoC/EmployeeImpl.java
+++ SpringOne/src/cl/vicoga/IoC/EmployeeImpl.java
@@ -12,14 +12,20 @@ public class EmployeeImpl {
 	*/
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		/*Employee emp=context.getBean("meEmployee",Employee.class);
-		System.out.println(emp.getTask());
-		System.out.println(emp.getReport());
-		context.close();*/
+		EmployeeBoss empb=context.getBean("meEmployee",EmployeeBoss.class);
+		System.out.println(empb.getTask());
+		System.out.println(empb.getReport());
+		System.out.println(empb.getNameCompany());
+		System.out.println(empb.getEmail());
+		System.out.println(".........................");
+		
 		EmployeeClerk emp=context.getBean("MeEmployeeClerk",EmployeeClerk.class);
 		System.out.println(emp.getTask());
 		System.out.println(emp.getReport());
 		System.out.println(emp.getEmail());
+		System.out.println(emp.getNameCompany());
+		
+		context.close();
 	}
 
 }
