0607e989dd5720ea5a4bfee6e1b81ed80ea6d8ef
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-21 20:06:45 -0300

    Spring & Spring Boot Udemy Master commit 12

--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/models/entities/Client.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/models/entities/Client.java
@@ -2,8 +2,10 @@ package com.vicoga.di.app.models.entities;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Component
+@RequestScope
 public class Client {
 
 	@Value("${client.name}")
--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/models/entities/Invoice.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/models/entities/Invoice.java
@@ -1,16 +1,21 @@
 package com.vicoga.di.app.models.entities;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-public class Invoice {
+@RequestScope
+public class Invoice implements Serializable{
 
+	private static final long serialVersionUID = -1414985676828476086L;
 	@Value("${desc.invoice}")
 	private String description;
 	@Autowired
@@ -23,6 +28,8 @@ public class Invoice {
 	public void init() {
 	this.client.setName(this.client.getName().concat(" ").concat("xin"));
 	}
+@PreDestroy
+public void preDestroy() {System.out.println("destroy "+this.description);}
 
 	public String getDescription() {
 		return description;
