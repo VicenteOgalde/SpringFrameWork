6d69314bd517d48d3bd14f9a91657b4508f3a8bf
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-21 13:09:29 -0300

    Spring & Spring Boot Udemy Master commit 6

--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/controllers/IndexController.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.vicoga.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import com.vicoga.di.app.models.services.IService;
 public class IndexController {
 
 	@Autowired
+	@Qualifier("firstService")
 	private IService service;
 
 	@GetMapping({"/","","/index"})
