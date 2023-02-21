dd4d2c6ae28fd49e5347d16e12e31b5f86f549ef
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-20 21:53:59 -0300

    Spring & Spring Boot Udemy Master commit 1

--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/controllers/IndexController.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/controllers/IndexController.java
@@ -1,5 +1,18 @@
 package com.vicoga.di.app.controllers;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.vicoga.di.app.models.services.MeService;
+
+@Controller
+public class IndexController {
+	private MeService service= new MeService();
+
+	@GetMapping({"/","","/index"})
+	public String index(Model model) {
+		model.addAttribute("object",service.operation());
+		return "index";
+	}
 }
---
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-di/src/main/java/com/vicoga/di/app/models/services/MeService.java
@@ -0,0 +1,9 @@
+package com.vicoga.di.app.models.services;
+
+public class MeService {
+	
+	public String operation() {
+		return"some process";
+	}
+
+}
