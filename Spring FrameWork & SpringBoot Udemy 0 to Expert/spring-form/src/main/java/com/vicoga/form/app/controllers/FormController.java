646b5cf0dd8b780e24c168305f3de57297c31b22
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-22 11:26:06 -0300

    Spring & Spring Boot Udemy Master commit 14

--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/controllers/FormController.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/controllers/FormController.java
@@ -1,5 +1,21 @@
 package com.vicoga.form.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class FormController {
+	@GetMapping("/form")
+	public String form(Model model) {
+		
+		
+		return "form";
+	}
+	@PostMapping
+	public String formPost(Model model) {
+		return"result";
+	}
 
 }
