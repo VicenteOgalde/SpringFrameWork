b9523727b7010cdb02ee52b504accf8c4b9d3131
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-22 11:12:03 -0300

    Spring & Spring Boot Udemy Master commit 13

---
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/SpringFormApplication.java
@@ -0,0 +1,13 @@
+package com.vicoga.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormApplication.class, args);
+	}
+
+}
---
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/controllers/FormController.java
@@ -0,0 +1,5 @@
+package com.vicoga.form.app.controllers;
+
+public class FormController {
+
+}
