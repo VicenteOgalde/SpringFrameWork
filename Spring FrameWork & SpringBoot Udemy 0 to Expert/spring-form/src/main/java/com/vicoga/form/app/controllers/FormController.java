50e88cd7cbf7d1ce0deb5a84e001077331d7f084
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-23 09:47:11 -0300

    Spring & Spring Boot Udemy Master commit 23

--- Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/controllers/FormController.java
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/controllers/FormController.java
@@ -4,17 +4,31 @@ package com.vicoga.form.app.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 import com.vicoga.form.app.models.User;
+import com.vicoga.form.app.validation.UserValidation;
 
 @Controller
 public class FormController {
+	
+	@Autowired
+	private UserValidation validation;
+	
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validation);
+	}
+	
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("title", "Form thymeleaf");
@@ -24,6 +38,8 @@ public class FormController {
 	}
 	@PostMapping("/form")
 	public String formPost(@Valid User user,BindingResult result, Model model) {
+		
+		//validation.validate(user, result);
 		
 		if(result.hasErrors()) {
 			/*
---
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-form/src/main/java/com/vicoga/form/app/validation/UserValidation.java
@@ -0,0 +1,26 @@
+package com.vicoga.form.app.validation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.vicoga.form.app.models.User;
+
+@Component
+public class UserValidation implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		
+		return User.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		User user=(User)target;
+		ValidationUtils.rejectIfEmpty(errors, "name", "NotEmpty.user.name");
+		
+	}
+
+}
