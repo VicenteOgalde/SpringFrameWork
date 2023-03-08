67967de1ac796c0ad1b6d69a1b940137c6cf2b41
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-08 09:43:44 -0300

    Spring security 0 to Master 1

---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-basic/src/main/java/com/vicoga/springsecuritybasic/SpringSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.vicoga.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicApplication.class, args);
+	}
+
+}
---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-basic/src/main/java/com/vicoga/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.vicoga.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+@GetMapping("/welcome")
+public String welcome(){
+    return"welcome spring";
+}
+
+}
