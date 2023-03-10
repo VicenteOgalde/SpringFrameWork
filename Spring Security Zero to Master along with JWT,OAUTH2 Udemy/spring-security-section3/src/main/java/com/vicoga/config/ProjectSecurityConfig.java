40d255e8e907e28ff514ae9d133993d46998c802
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-09 22:17:17 -0300

    Spring security 0 to Master 5

--- Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +20,20 @@ public class ProjectSecurityConfig {
                         .permitAll().and().formLogin().and().httpBasic();
 
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetails(){
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .authorities("admin")
+                .build();
+        UserDetails user= User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/controller/BalanceController.java
@@ -0,0 +1,15 @@
+package com.vicoga.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    @GetMapping("/my-balance")
+    public String getBalance(){
+        return "balance here";
+    }
+
+
+}
