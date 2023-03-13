eb9a87a8317f017c059ecadc321af871dc36849a
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-12 20:18:03 -0400

    Spring security 0 to Master 7

--- Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
@@ -6,8 +6,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -21,6 +27,7 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetails(){
         UserDetails admin= User.withDefaultPasswordEncoder()
@@ -35,5 +42,14 @@ public class ProjectSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(admin,user);
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
