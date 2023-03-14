58e02d82325807ddb8da791f7412e9adf0fcd408
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-13 23:40:59 -0400

    Spring security 0 to Master 8

---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section3/src/main/java/com/vicoga/model/Customer.java
@@ -0,0 +1,48 @@
+package com.vicoga.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String email;
+    private String pass;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
