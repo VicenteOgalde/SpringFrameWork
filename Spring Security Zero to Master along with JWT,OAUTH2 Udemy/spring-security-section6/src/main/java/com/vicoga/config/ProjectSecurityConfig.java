413c1a3aeef6a2ad28b9407cd619679d54b53dfb
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-16 13:27:07 -0400

    Spring security 0 to Master 14

--- Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section6/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section6/src/main/java/com/vicoga/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vicoga.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -22,7 +26,19 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config=new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and()
+                .csrf().disable()
                 .authorizeHttpRequests().requestMatchers("/my-account","/my-cards","/my-balance","/my-loans","/user").authenticated()
                 .requestMatchers("/contact","/notices","/register").permitAll()
                 .and().formLogin()
