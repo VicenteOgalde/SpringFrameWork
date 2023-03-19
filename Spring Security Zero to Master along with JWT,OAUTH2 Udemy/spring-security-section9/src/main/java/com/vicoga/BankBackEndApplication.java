cbd27f40a64a7a838c25ecf661901fcf7a760de2
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-19 14:05:04 -0400

    Spring security 0 to Master 25

--- Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section9/src/main/java/com/vicoga/BankBackEndApplication.java
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section9/src/main/java/com/vicoga/BankBackEndApplication.java
@@ -2,9 +2,9 @@ package com.vicoga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity(debug=true)
+
+
 @SpringBootApplication
 public class BankBackEndApplication {
 
