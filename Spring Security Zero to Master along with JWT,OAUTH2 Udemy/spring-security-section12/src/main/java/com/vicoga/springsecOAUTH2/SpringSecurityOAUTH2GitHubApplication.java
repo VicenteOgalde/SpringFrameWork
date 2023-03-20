a7bca6a72c75ed75651d32534069f4e161e3be2b
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-20 17:06:36 -0400

    Spring security 0 to Master 28

---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section12/src/main/java/com/vicoga/springsecOAUTH2/SpringSecurityOAUTH2GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.vicoga.springsecOAUTH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAUTH2GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAUTH2GitHubApplication.class, args);
+	}
+
+}
---
+++ Spring Security Zero to Master along with JWT,OAUTH2 Udemy/spring-security-section12/src/test/java/com/vicoga/springsecOAUTH2/SpringSecurityOauth2GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.springsecOAUTH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
