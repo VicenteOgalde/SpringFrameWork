7bc9e2fa1b16918b2b20aecbc92ea055b61f5075
vicente_ogalde <vicenteogalde@gmail.com>
2023-03-29 11:09:05 -0400

    Spring Framework & Spring boot Udemy commit 73

---
+++ Spring FrameWork & SpringBoot Udemy 0 to Expert/spring-webflux/src/main/java/com/vicoga/webflux/app/controllers/ProductController.java
@@ -0,0 +1,37 @@
+package com.vicoga.webflux.app.controllers;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+import com.vicoga.webflux.app.models.dao.ProductDao;
+import com.vicoga.webflux.app.models.documents.Product;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductController {
+	
+	@Autowired
+	private ProductDao repository;
+	
+	@GetMapping({"/list","/"})
+	public String productList(Model model) {
+		Flux<Product>products=repository.findAll();
+		model.addAttribute("products", products);
+		model.addAttribute("title", "Products List");
+		return "list";
+	}
+	
+	@GetMapping("/list-datadriver")
+	public String listDataDriver(Model model) {
+		Flux<Product>products=repository.findAll().delayElements(Duration.ofSeconds(1));
+		model.addAttribute("products",new ReactiveDataDriverContextVariable(products, 1));
+		model.addAttribute("title", "Products List");
+		return "list";
+	}
+}
