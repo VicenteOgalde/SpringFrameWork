4908da20ce2112897cb3e7a03599ab26fa8453d1
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-01 13:36:43 -0400

    Reactive Programming with Spring Framework & Spring boot Udemy commit 02

--- Reactive Programming with Spring Boot and Spring WebFlux Udemy/spring-boot-webflux-api-rest/src/main/java/com/vicoga/webflux/controllers/ProductController.java
+++ Reactive Programming with Spring Boot and Spring WebFlux Udemy/spring-boot-webflux-api-rest/src/main/java/com/vicoga/webflux/controllers/ProductController.java
@@ -1,5 +1,31 @@
 package com.vicoga.webflux.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vicoga.webflux.models.documents.Product;
+import com.vicoga.webflux.models.services.ProductService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping
+	public Mono<ResponseEntity<Flux<Product>>> list(){
+		return Mono.just(
+				ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(productService.findAll()));
+	}
 
 }
