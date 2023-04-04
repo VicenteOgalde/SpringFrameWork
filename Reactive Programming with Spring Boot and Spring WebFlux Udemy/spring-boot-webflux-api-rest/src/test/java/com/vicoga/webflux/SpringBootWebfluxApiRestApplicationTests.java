4b319c78732832eb3dd89783dda8963d0acbcf28
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-04 19:45:48 -0400

    Spring security 0 to Master 29

--- Reactive Programming with Spring Boot and Spring WebFlux Udemy/spring-boot-webflux-api-rest/src/test/java/com/vicoga/webflux/SpringBootWebfluxApiRestApplicationTests.java
+++ Reactive Programming with Spring Boot and Spring WebFlux Udemy/spring-boot-webflux-api-rest/src/test/java/com/vicoga/webflux/SpringBootWebfluxApiRestApplicationTests.java
@@ -1,13 +1,30 @@
 package com.vicoga.webflux;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.vicoga.webflux.models.documents.Product;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApiRestApplicationTests {
+	
+	@Autowired
+	WebTestClient client;
 
 	@Test
-	void contextLoads() {
+	void ListTest() {
+		
+		client.get()
+		.uri("/api/v2/products")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Product.class)
+		.hasSize(9);
 	}
 
 }
