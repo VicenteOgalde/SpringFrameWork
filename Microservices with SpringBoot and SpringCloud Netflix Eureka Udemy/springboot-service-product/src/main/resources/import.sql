fdd1a20d50c2e0f1693321bd4dabbed8f58fe8c5
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-07 19:37:19 -0400

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 04

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (name, price, created_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Sony', 700, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Apple', 1000, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Sony Notebook', 1000, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Hewlett Packard', 500, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Bianchi', 600, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Nike', 100, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Adidas', 200, NOW());
+INSERT INTO products (name, price, created_at) VALUES('Reebok', 300, NOW());
