c5b4db407bd591e13750601b0413bc2ae1789232
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-06 21:26:06 -0400

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 01

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/java/com/vicoga/product/models/entity/Product.java
@@ -0,0 +1,59 @@
+package com.vicoga.product.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable{
+	
+	
+	private static final long serialVersionUID = -5286044134335007974L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	
+	@Column(name = "created_at")
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+
+}
