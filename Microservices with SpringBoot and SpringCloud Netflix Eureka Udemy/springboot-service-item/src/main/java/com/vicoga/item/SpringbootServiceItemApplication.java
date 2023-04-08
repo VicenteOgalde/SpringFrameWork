90a92d6db9a172b6fa04110a6e4c0f1ea3b13ef2
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-08 18:06:08 -0400

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 11

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-item/src/main/java/com/vicoga/item/SpringbootServiceItemApplication.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-item/src/main/java/com/vicoga/item/SpringbootServiceItemApplication.java
@@ -2,8 +2,10 @@ package com.vicoga.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServiceItemApplication {
 
--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/java/com/vicoga/product/SpringbootServiceProductApplication.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/java/com/vicoga/product/SpringbootServiceProductApplication.java
@@ -2,7 +2,8 @@ package com.vicoga.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServiceProductApplication {
 
