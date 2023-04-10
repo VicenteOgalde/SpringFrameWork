180ee3a173627fa5bb615c40c9351ebb2eaeec41
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-10 17:20:04 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 13

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-gateway-server/src/test/java/com/vicoga/gateway/SpringbootServiceGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
