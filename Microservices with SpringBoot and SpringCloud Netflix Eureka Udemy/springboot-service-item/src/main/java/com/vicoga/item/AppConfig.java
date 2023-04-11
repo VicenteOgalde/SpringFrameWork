b056df09124baf4c59ca72517489ed3134fc641f
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-11 09:13:25 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 20

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-item/src/main/java/com/vicoga/item/AppConfig.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-item/src/main/java/com/vicoga/item/AppConfig.java
@@ -1,8 +1,16 @@
 package com.vicoga.item;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +18,21 @@ public class AppConfig {
 	@Bean("restClient")
 	public RestTemplate restTemplateRegister() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id->{
+			return new Resilience4JConfigBuilder(id)
+					.circuitBreakerConfig(CircuitBreakerConfig.custom()
+							.slidingWindowSize(10)
+							.failureRateThreshold(50)//% of failure 
+							.waitDurationInOpenState(Duration.ofSeconds(10L))//duration of the alternative path
+							.permittedNumberOfCallsInHalfOpenState(5)
+							.build())
+					.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+					.build();
+		});
 	}
 	
 	
