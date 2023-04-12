961fd7d76deda2f4b623d63b7c78f0d4353fa999
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-11 21:10:31 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 26

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-config-server/src/test/java/com/vicoga/config/SpringbootServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
