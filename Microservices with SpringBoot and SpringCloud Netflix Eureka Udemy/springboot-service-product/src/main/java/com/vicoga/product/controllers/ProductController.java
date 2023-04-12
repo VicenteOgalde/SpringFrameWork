0c70158ef7db132023ff9cac4f9dd27496e76983
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-12 16:27:32 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 32

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/java/com/vicoga/product/controllers/ProductController.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-product/src/main/java/com/vicoga/product/controllers/ProductController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +38,23 @@ public class ProductController {
 	public Product create(@RequestBody Product product) {
 		
 		return service.save(product);
+	}
+	
+	@PutMapping("/edit/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product update(@RequestBody Product product,@PathVariable Long id) {
+		
+		Product p= service.findById(id);
+		p.setName(product.getName());
+		p.setPrice(product.getPrice());
+		return service.save(p);
+		
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
 	}
 
 }
