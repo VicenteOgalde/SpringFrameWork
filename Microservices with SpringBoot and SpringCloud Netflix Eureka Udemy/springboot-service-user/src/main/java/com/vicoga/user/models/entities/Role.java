a152ac1012eb2c9e2dd973e31611198fe913e11c
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-13 20:00:56 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 40

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/java/com/vicoga/user/models/entities/Role.java
@@ -0,0 +1,40 @@
+package com.vicoga.user.models.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = -3072417827253455407L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, length = 30)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/java/com/vicoga/user/models/entities/User.java
@@ -0,0 +1,86 @@
+package com.vicoga.user.models.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 8933033553094218470L;
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, length = 25)
+	private String username;
+	@Column(length = 60)
+	private String password;
+	private Boolean enabled;
+	private String name;
+	private String surname;
+	@Column(unique = true, length = 100)
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
