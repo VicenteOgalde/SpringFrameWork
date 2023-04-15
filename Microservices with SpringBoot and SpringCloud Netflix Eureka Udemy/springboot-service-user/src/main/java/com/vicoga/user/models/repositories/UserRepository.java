cbc950e724eb0e97e7609bf8f66d3f8de41815cd
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-14 21:28:23 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 44

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/java/com/vicoga/user/models/repositories/UserRepository.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/java/com/vicoga/user/models/repositories/UserRepository.java
@@ -1,12 +1,15 @@
 package com.vicoga.user.models.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.vicoga.user.models.entities.User;
 @RepositoryRestResource(path = "v01")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-	public User findByUsername(String username);
+	@RestResource(path = "find-username")
+	public User findByUsername(@Param("username")String username);
 	
 }
