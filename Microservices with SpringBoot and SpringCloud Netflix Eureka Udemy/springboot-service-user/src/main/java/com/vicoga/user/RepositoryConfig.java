537f54a6c84368f59abcab74c7f599b963fc3f63
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-14 21:34:21 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 45

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/java/com/vicoga/user/RepositoryConfig.java
@@ -0,0 +1,23 @@
+package com.vicoga.user;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.vicoga.user.models.entities.Role;
+import com.vicoga.user.models.entities.User;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		
+		config.exposeIdsFor(User.class,Role.class);
+		
+	}
+	
+	
+
+}
