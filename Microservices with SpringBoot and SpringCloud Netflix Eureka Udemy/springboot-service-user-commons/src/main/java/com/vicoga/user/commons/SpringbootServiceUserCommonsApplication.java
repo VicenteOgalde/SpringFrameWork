099c16cd6982d6fe2c32afc14c3063c96a7138db
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-15 20:28:44 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 46

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user-commons/src/main/java/com/vicoga/user/commons/SpringbootServiceUserCommonsApplication.java
@@ -0,0 +1,12 @@
+package com.vicoga.user.commons;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServiceUserCommonsApplication {
+
+}
---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user-commons/src/test/java/com/vicoga/user/commons/SpringbootServiceUserCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.user.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceUserCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
