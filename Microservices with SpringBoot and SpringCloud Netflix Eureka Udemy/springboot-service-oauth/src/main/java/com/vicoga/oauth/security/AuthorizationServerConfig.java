64c0b2acba143dfd0a9a2974b1b6e39b86f254f1
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-16 15:39:04 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 53

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-oauth/src/main/java/com/vicoga/oauth/security/AuthorizationServerConfig.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-oauth/src/main/java/com/vicoga/oauth/security/AuthorizationServerConfig.java
@@ -26,14 +26,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(security);
+		
+		security.tokenKeyAccess("permitAll()")
+		.checkTokenAccess("isAuthenticated()");
+		
+		
+		
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(clients);
+		
+		clients.inMemory()
+		.withClient("FrontEndApp")
+		.secret(passwordEncoder.encode("123"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password","refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
 	}
 
 	@Override
@@ -48,14 +58,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Bean
 	public JwtTokenStore tokenStore() {
-		
-		
 		return new JwtTokenStore(accessTokenConverter());
 	}
 
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
-		
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey("123");
 		return tokenConverter;
