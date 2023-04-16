1269f1791979901b3fc46007ace9a4c49500a144
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-16 13:23:41 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 50

---
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-oauth/src/main/java/com/vicoga/oauth/services/UserService.java
@@ -0,0 +1,40 @@
+package com.vicoga.oauth.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vicoga.commons.user.models.entities.User;
+import com.vicoga.oauth.clients.UserFeignClient;
+@Service
+public class UserService implements UserDetailsService{
+	
+	private UserFeignClient client;
+
+	public UserService(UserFeignClient client) {
+		this.client=client;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user= client.findByUsername(username);
+		if(user==null) {
+			throw new UsernameNotFoundException(username.concat(" not found in the database."));
+		}
+		List<GrantedAuthority> roles=user.getRoles()
+				.stream()
+				.map(r->new SimpleGrantedAuthority(r.getName()))
+				.collect(Collectors.toList());
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getName(), user.getPassword(), user.getEnabled(),
+				true, true, true, roles);
+	}
+
+}
