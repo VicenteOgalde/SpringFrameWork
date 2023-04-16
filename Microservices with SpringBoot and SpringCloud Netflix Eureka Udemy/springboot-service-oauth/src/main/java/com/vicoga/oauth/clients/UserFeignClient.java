1df8c3f78c159fe30b977cfc4ee2580a7c384d91
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-16 13:02:53 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 49

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-oauth/src/main/java/com/vicoga/oauth/clients/UserFeignClient.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-oauth/src/main/java/com/vicoga/oauth/clients/UserFeignClient.java
@@ -2,10 +2,14 @@ package com.vicoga.oauth.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.vicoga.commons.user.models.entities.User;
 
 @FeignClient(name = "user-service")
-public class UserFeignClient {
+public interface UserFeignClient {
 
-	
+	@GetMapping("/v01/search/find-username")
+	public User findByUsername(@RequestParam String username);
 	
 }
