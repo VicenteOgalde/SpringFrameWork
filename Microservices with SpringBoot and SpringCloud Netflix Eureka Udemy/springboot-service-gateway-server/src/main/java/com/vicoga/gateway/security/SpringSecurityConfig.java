1ec360c971c641fcc11189313f60011ac106d122
vicente_ogalde <vicenteogalde@gmail.com>
2023-04-17 21:54:43 -0300

    Microservices SpringBoot & SpringCloud Netflix Eureka Udemy commit 57

--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-gateway-server/src/main/java/com/vicoga/gateway/security/SpringSecurityConfig.java
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-gateway-server/src/main/java/com/vicoga/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vicoga.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,6 +13,14 @@ public class SpringSecurityConfig {
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
 		return http
 				.authorizeExchange()
+				.pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET,"/api/products/list"
+						,"/api/products/show/{id}"
+						,"/api/items/list"
+						,"/api/items/show/{id}/amount/{amount}"
+						,"/api/users/v01").permitAll()
+				.pathMatchers(HttpMethod.GET,"/api/users/v01/{id}").hasAnyRole("ADMIN","USER")
+				.pathMatchers("/api/items/**","/api/products/**","/api/users/v01/**").hasRole("ADMIN")
 				.anyExchange().authenticated()
 				.and().csrf().disable()
 				.build();
--- Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/resources/import.sql
+++ Microservices with SpringBoot and SpringCloud Netflix Eureka Udemy/springboot-service-user/src/main/resources/import.sql
@@ -4,6 +4,6 @@ INSERT INTO users (username, password, enabled,name,surname,email) VALUES('user2
 INSERT INTO roles (name) VALUES('ROLE_ADMIN');
 INSERT INTO roles (name) VALUES('ROLE_USER');
 
-INSERT INTO users_roles (user_id, role_id) VALUES( 1,2);
-INSERT INTO users_roles (user_id, role_id) VALUES( 2,1);
+INSERT INTO users_roles (user_id, role_id) VALUES( 1,1);
 INSERT INTO users_roles (user_id, role_id) VALUES( 2,2);
+INSERT INTO users_roles (user_id, role_id) VALUES( 1,2);
