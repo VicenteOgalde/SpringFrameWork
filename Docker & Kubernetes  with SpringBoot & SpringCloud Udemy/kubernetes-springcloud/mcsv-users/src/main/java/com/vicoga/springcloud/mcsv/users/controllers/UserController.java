3edeeb8abdce1cdaf0c6e65d0539637d99f5dfcf
Vicente Ogalde <vicenteogalde@gmail.com>
2023-09-19 21:30:25 +1200

    commit 5

--- Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-users/src/main/java/com/vicoga/springcloud/mcsv/users/controllers/UserController.java
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-users/src/main/java/com/vicoga/springcloud/mcsv/users/controllers/UserController.java
@@ -3,10 +3,9 @@ package com.vicoga.springcloud.mcsv.users.controllers;
 import com.vicoga.springcloud.mcsv.users.models.entities.User;
 import com.vicoga.springcloud.mcsv.users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,4 +28,23 @@ public class UserController {
         }
         return ResponseEntity.notFound().build();
     }
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> edit(@RequestBody User user,@PathVariable Long id){
+        Optional<User> optionalUser= userService.findById(id);
+        if(optionalUser.isPresent()){
+            User userDB= optionalUser.get();
+            userDB.setName(user.getName());
+            userDB.setEmail(user.getEmail());
+            userDB.setPassword(user.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userDB));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
