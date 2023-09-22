210e7835a344743bd3e52fa41877c47087ae4545
Vicente Ogalde <vicenteogalde@gmail.com>
2023-09-22 20:24:16 +1200

    commit 8

---
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/McsvCourseApplication.java
@@ -0,0 +1,13 @@
+package com.vicoga.springcloud.mcsv.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvCourseApplication.class, args);
+	}
+
+}
---
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/test/java/com/vicoga/springcloud/mcsv/course/McsvCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.springcloud.mcsv.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
