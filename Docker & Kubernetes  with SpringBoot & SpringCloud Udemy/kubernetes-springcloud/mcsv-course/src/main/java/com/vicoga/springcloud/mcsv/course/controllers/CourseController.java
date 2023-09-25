a6c62e1c623a90dcf7a851b5aafe431d3e991a23
Vicente Ogalde <vicenteogalde@gmail.com>
2023-09-25 21:23:42 +1300

    commit 11

--- Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/controllers/CourseController.java
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/controllers/CourseController.java
@@ -5,9 +5,11 @@ import com.vicoga.springcloud.mcsv.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
@@ -18,6 +20,13 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<List<Course>> list(){
         return ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> details(@PathVariable Long id){
+        Optional<Course> o=service.findById(id);
+        if(o.isPresent()){
+            return ResponseEntity.ok(o.get());
+        }
     }
 
 }
