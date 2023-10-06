dec6e3d9a3b1884d8b1a2c54a643ab509f55f21e
Vicente Ogalde <vicenteogalde@gmail.com>
2023-10-06 18:03:10 +1300

    commit 16

--- Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/entity/Course.java
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/entity/Course.java
@@ -3,6 +3,9 @@ package com.vicoga.springcloud.mcsv.course.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "courses")
 public class Course {
@@ -11,6 +14,12 @@ public class Course {
     private Long id;
     @NotBlank
     private String name;
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CourseUser> courseUsers;
+
+    public Course() {
+        courseUsers= new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -26,5 +35,19 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void addCourseUser(CourseUser courseUser){
+        this.courseUsers.add(courseUser);
+    }
+    public void deleteCourseUser(CourseUser courseUser){
+        this.courseUsers.remove(courseUser);
+    }
+    public List<CourseUser> getCourseUsers() {
+        return courseUsers;
+    }
+
+    public void setCourseUsers(List<CourseUser> courseUsers) {
+        this.courseUsers = courseUsers;
     }
 }
