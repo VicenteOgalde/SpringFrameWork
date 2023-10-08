d0a53e946f595fd723c9cd0256213809f401465a
Vicente Ogalde <vicenteogalde@gmail.com>
2023-10-08 18:15:09 +1300

    commit 20

--- Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/services/CourseServiceImpl.java
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/services/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.vicoga.springcloud.mcsv.course.services;
 import com.vicoga.springcloud.mcsv.course.clients.UserClientRest;
 import com.vicoga.springcloud.mcsv.course.models.User;
 import com.vicoga.springcloud.mcsv.course.models.entity.Course;
+import com.vicoga.springcloud.mcsv.course.models.entity.CourseUser;
 import com.vicoga.springcloud.mcsv.course.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,19 +38,59 @@ public class CourseServiceImpl implements CourseService {
     public void deleteById(Long id) {
         repository.deleteById(id);
     }
-
+    @Transactional
     @Override
-    public Optional<User> assignUser(User user, Long id) {
+    public Optional<User> assignUser(User user, Long courseId) {
+
+        Optional<Course> optionalCourse= repository.findById(courseId);
+        if(optionalCourse.isPresent()){
+            User userMcsv= userClientRest.details(user.getId());
+            Course course= optionalCourse.get();
+            CourseUser courseUser= new CourseUser();
+            courseUser.setUserId(userMcsv.getId());
+            course.addCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(userMcsv);
+
+        }
+
         return Optional.empty();
     }
-
+    @Transactional
     @Override
-    public Optional<User> createUser(User user, Long courseId) {
-        return Optional.empty();
-    }
+    public Optional<User> createUser(User user, Long courseId)
+    {
+        Optional<Course> optionalCourse= repository.findById(courseId);
+        if(optionalCourse.isPresent()){
+            User newUserMcsv= userClientRest.create(user);
+            Course course= optionalCourse.get();
+            CourseUser courseUser= new CourseUser();
+            courseUser.setUserId(newUserMcsv.getId());
+            course.addCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(newUserMcsv);
 
+        }
+
+        return Optional.empty();
+
+    }
+    @Transactional
     @Override
     public Optional<User> unAssignUser(User user, Long courseId) {
+
+        Optional<Course> optionalCourse= repository.findById(courseId);
+        if(optionalCourse.isPresent()){
+            User userMcsv= userClientRest.details(user.getId());
+            Course course= optionalCourse.get();
+            CourseUser courseUser= new CourseUser();
+            courseUser.setUserId(userMcsv.getId());
+            course.deleteCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(userMcsv);
+
+        }
+
         return Optional.empty();
     }
 }
