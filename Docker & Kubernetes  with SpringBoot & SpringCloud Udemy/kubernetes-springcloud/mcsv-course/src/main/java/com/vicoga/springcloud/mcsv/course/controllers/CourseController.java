417aa27438104b9077f8c152eb594d7714f74e00
Vicente Ogalde <vicenteogalde@gmail.com>
2023-10-09 18:35:14 +1300

    commit 21

--- Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/controllers/CourseController.java
+++ Docker & Kubernetes  with SpringBoot & SpringCloud Udemy/kubernetes-springcloud/mcsv-course/src/main/java/com/vicoga/springcloud/mcsv/course/controllers/CourseController.java
@@ -1,7 +1,9 @@
 package com.vicoga.springcloud.mcsv.course.controllers;
 
+import com.vicoga.springcloud.mcsv.course.models.User;
 import com.vicoga.springcloud.mcsv.course.models.entity.Course;
 import com.vicoga.springcloud.mcsv.course.services.CourseService;
+import feign.FeignException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,10 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class CourseController {
@@ -64,6 +63,86 @@ public class CourseController {
         return ResponseEntity.notFound().build();
 
     }
+
+    @PutMapping("/user-assign/{courseId}")
+    public ResponseEntity<?> userAssign(@RequestBody User user, @PathVariable Long courseId){
+        Optional<User> userOp;
+        try {
+            userOp = service.assignUser(user, courseId);
+
+
+
+        }catch (FeignException feignException){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Collections.singletonMap("Message", "Wrong user id or " +
+                            "service error: " + feignException.getMessage()));
+
+        }
+
+        if(userOp.isPresent()){
+            return  ResponseEntity.status(HttpStatus.CREATED).body(userOp.get());
+        }
+
+
+        return ResponseEntity.notFound().build();
+
+
+
+    }
+
+    @PostMapping("/user-create/{courseId}")
+    public ResponseEntity<?> userCreate(@RequestBody User user, @PathVariable Long courseId){
+        Optional<User> userOp;
+        try {
+            userOp = service.createUser(user, courseId);
+
+
+
+        }catch (FeignException feignException){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Collections.singletonMap("Message", "We cant create user or " +
+                            "service error: " + feignException.getMessage()));
+
+        }
+
+        if(userOp.isPresent()){
+            return  ResponseEntity.status(HttpStatus.CREATED).body(userOp.get());
+        }
+
+
+        return ResponseEntity.notFound().build();
+
+
+
+    }
+
+    @DeleteMapping("/user-delete/{courseId}")
+    public ResponseEntity<?> userDelete(@RequestBody User user, @PathVariable Long courseId){
+        Optional<User> userOp;
+        try {
+            userOp = service.unAssignUser(user, courseId);
+
+
+
+        }catch (FeignException feignException){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Collections.singletonMap("Message", "Wrong user or " +
+                            "service error: " + feignException.getMessage()));
+
+        }
+
+        if(userOp.isPresent()){
+            return  ResponseEntity.status(HttpStatus.OK).body(userOp.get());
+        }
+
+
+        return ResponseEntity.notFound().build();
+
+
+
+    }
+
+
     private static ResponseEntity<Map<String, String>> validation(BindingResult result) {
         Map<String,String>errors= new HashMap<>();
         result.getFieldErrors().forEach(err->{
